7bcd227b1525d24fe309306d2bb3fc78de977938
jakubrejch <jrejch@citizengo.net>
2026-06-03 09:58:07 +0200

    add scss

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,6 +1,6 @@
 // app/layout.jsx
 // Layout opakowuje kazdą stronę.. tu trafiają <html>, fonty, nagłówek, stopka
-import "./globals.css";
+import "./globals.scss";
 
 export const metadata = {
     title: "Neonatologia Rzeszów",
@@ -10,6 +10,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="pl">
+            <head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,400;9..144,600;9..144,900&family=Newsreader:ital,opsz,wght@0,6..72,400;0,6..72,500;1,6..72,400&display=swap"
+                    rel="stylesheet"
+                />
+            </head>
             <body>
                 <header className="site-header">
                     <a href="/" className="site-header_logo">Logo</a>
